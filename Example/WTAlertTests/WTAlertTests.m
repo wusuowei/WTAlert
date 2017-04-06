2c2a6d063a5d0b005b732969e85bbc4bbed33b8e
wentianen <wentianen@xingshulin.com>
2017-04-06 20:48:32 +0800

    framework

---
+++ Example/WTAlertTests/WTAlertTests.m
@@ -0,0 +1,39 @@
+//
+//  WTAlertTests.m
+//  WTAlertTests
+//
+//  Created by 温天恩 on 2017/4/6.
+//  Copyright © 2017年 wentianen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WTAlertTests : XCTestCase
+
+@end
+
+@implementation WTAlertTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
